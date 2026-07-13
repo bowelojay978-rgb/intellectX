56a6a586bef6f35ea02b67d4021ed90f1167a7fc
bowelojay978-rgb <bowelojay978@gmail.com>
2026-07-13 19:12:48 +0200

    Harden Capacitor WebView file access

--- android/app/src/main/java/com/intellectx/app/MainActivity.java
+++ android/app/src/main/java/com/intellectx/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.intellectx.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (bridge == null) {
+            return;
+        }
+
+        WebSettings settings = bridge.getWebView().getSettings();
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+        settings.setAllowFileAccessFromFileURLs(false);
+        settings.setAllowUniversalAccessFromFileURLs(false);
+    }
+}
